fe019f6de4913504ae789e795b85ee5c57953f9c
Nikhilbadakh <badakhng1999@gmail.com>
2024-03-07 17:33:47 +0530

    first commit

---
+++ assing1/demo4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+// star pattern 
+printf("\n*");
+printf("\n**");
+printf("\n***");
+printf("\n****");
+printf("\n*****");
+
+// star pattern using single printf
+printf("\n-----------------------------------------------------------------");
+
+printf("\n*\n**\n***\n****\n*****\n******");
+
+
+
+
+
+return 0;
+}
---
+++ assing1/demo5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+int num1;
+//Ascii value for \n,\r,\t,\a,\b
+
+printf("\n ascii value of '\n'=%d");
+printf("\n ascii value of '\r'=%d");
+printf("\n ascii value of '\t'=%d");
+printf("\n ascii value of '\a'=%d");
+printf("\n ascii value of '\b'=%d");
+
+
+
+return 0;
+}
