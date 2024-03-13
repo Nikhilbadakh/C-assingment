0f10242ad171df77a3aa54cca37739a015b0df02
Nikhilbadakh <badakhng1999@gmail.com>
2024-03-13 13:47:21 +0530

    assingment no.1

--- assing1/demo5.c
+++ assing1/demo5.c
@@ -5,11 +5,11 @@ int main()
 int num1;
 //Ascii value for \n,\r,\t,\a,\b
 
-printf("\n ascii value of '\n'=%d");
-printf("\n ascii value of '\r'=%d");
-printf("\n ascii value of '\t'=%d");
-printf("\n ascii value of '\a'=%d");
-printf("\n ascii value of '\b'=%d");
+printf("\n ascii value of \\n=%d", '\n');
+printf("\n ascii value of \\r=%d", '\r');
+printf("\n ascii value of \\t=%d", '\t');
+printf("\n ascii value of \\a=%d", '\a');
+printf("\n ascii value of \\b=%d",  '\b');
 
 
 
---
+++ assing1/demo9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+  
+   int main()
+   /*Write a program to convert temperature in Celsius to Fahrenheit and vice versa. Formula for
+   conversion*/
+   {
+   float celsius,fahrenheit ,c ;
+   
+  printf("\n Enter the temperature in celsius  :");
+  
+  scanf("%d",&celsius);
+  c=(5/9*(fahrenheit-32));
+  printf("\n the fahrenheit is =%d ",c);
+  
+  
+  return 0;
+  }
+  
+
